d873c00978a0d24a220fcf31c7dd9d65f60fac54
alexbalash <55133359+alexbalash@users.noreply.github.com>
2020-01-17 13:57:49 +0300

    Delete main.js

--- _ENCYCLO/js/views/main.js
+++
@@ -1,7 +0,0 @@
-class MainView extends BaseView {
-	update() {
-		let html = '<h1>Энциклопедия</h1>';
-		html += '<a data-url = "contents">список статей</a>';
-		this.container.innerHTML = html;
-	}
-}
